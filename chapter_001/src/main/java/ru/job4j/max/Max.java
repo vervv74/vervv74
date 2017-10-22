6d721019bada91dbbe7b13f63ac0bf010f92bf60
Valentin Verkhovykh <mailto:vervv74@gmail.com>
2017-10-22 21:57:21 +0300

    3.1. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,19 @@
+package ru.job4j.max;
+
+/**
+ * @author Valentin Verkhovykh (mailto:vervv74@gmail.com)
+ * @since 21.10.17 ggg
+ */
+public class Max {
+    /**
+     * @param first  первое слагаемое
+     * @param second второе слагаемое
+     * @return максимум
+     * @author Valentin Verkhovykh (mailto:vervv74@gmail.com)
+     * Method add
+     * @since 20.10.17 новый
+     */
+    public int max(int first, int second) {
+        return first >= second ? first : second;
+    }
+}
