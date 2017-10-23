a92d4d0bef06ed058b346bc1ce355e7637841762
vervv <vervv74@gmail.com>
2017-10-23 13:59:22 +0300

    3.3. Вычисление площади треугольника [#9461]

---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.*;
+
+/**
+ * Created by v.verkhovykh on 23.10.2017.
+ */
+
+public class TriangleTest {
+    /**
+     * @author Valentin Verkhovykh (mailto:vervv74@gmail.com)
+     * @since 23.10.17
+     */
+    @Test
+    /**
+     * @author Valentin Verkhovykh (mailto:vervv74@gmail.com)
+     * @since 23.10.17
+     */
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        // создаем три объекта класса Point.
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 0);
+        // Создаем объект треугольник и передаем в него объекты точек.
+        Triangle triangle = new Triangle(a, b, c);
+        // Вычисляем площадь.
+        double result = triangle.area();
+        // Задаем ожидаемый результат.
+        double expected = 2D;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, closeTo(expected, 0.1));
+    }
+    /**
+     * @author Valentin Verkhovykh (mailto:vervv74@gmail.com)
+     * @since 23.10.17
+     */
+    @Test
+    /**
+     * @author Valentin Verkhovykh (mailto:vervv74@gmail.com)
+     * @since 23.10.17
+     */
+    public void whenDistanceDiffPointsThen10() {
+        // создаем два объекта класса Point.
+        Point left = new Point(0, 0);
+        Point right = new Point(0, 10);
+        // Создаем объект треугольник и в качестве точек передает null.
+        // так как нам не требуется их участие.
+        Triangle triangle = new Triangle(null, null, null);
+
+        double rsl = triangle.distance(left, right);
+
+        assertThat(rsl, closeTo(10, 0.01));
+    }
+}
+
+
