9a3fc0f0057ec0738b04e87380d6d8e315a17aba
Valentin Verkhovykh <mailto:vervv74@gmail.com>
2017-10-25 03:14:36 +0300

    4.2. Создать программу вычисляющую факториал. [#193]2

--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -1,8 +1,7 @@
 package ru.job4j.loop;
 
 import org.junit.Test;
-
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 /**
