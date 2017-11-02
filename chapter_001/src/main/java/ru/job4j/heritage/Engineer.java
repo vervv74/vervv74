d5382828862139ba4221ff187180201c9b7c49ce
vervv <vervv74@gmail.com>
2017-11-02 14:52:03 +0300

    1. Реализация профессий в коде [#6837]

---
+++ chapter_001/src/main/java/ru/job4j/heritage/Engineer.java
@@ -0,0 +1,10 @@
+package ru.job4j.heritage;
+
+/**
+ * Created by V.Verkhovykh on 02.11.2017.
+ */
+public class Engineer extends Profession {
+    public void repair (Doctor doctor) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/heritage/Profession.java
@@ -0,0 +1,17 @@
+package ru.job4j.heritage;
+
+
+/**
+ * Created by V.Verkhovykh on 02.11.2017.
+ */
+public class Profession {
+    public String name;
+    public String education;
+    public String getName(){
+       return this.name;
+    }
+    public String getEducation(){
+        return this.education;
+    }
+
+}
